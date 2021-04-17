d79c65040c04dbc7679e2a38e99e7b93fd85d756
adrine <adrinekocharian@gmail.com>
2021-04-17 15:03:18 +0400

    Added ImageDownloader project.

---
+++ Practice2/ImageDownloader/ImageDownloader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Images
+{
+    class ImageDownloader
+    {
+        private byte[] imageBytes;
+        public async Task DownloadImageAsync(string url)
+        {
+            Console.WriteLine("Starting image download....");
+
+            HttpClient httpClient = new HttpClient();
+            this.imageBytes = await httpClient.GetByteArrayAsync(url);
+
+            //var result = httpClient.GetByteArrayAsync(url);
+            //this.imageBytes = result.Result;
+
+            Console.WriteLine("Successfully downloaded image.");
+        }
+
+        public async Task DownloadImageAsync(string url, CancellationToken token)
+        {
+            Thread.Sleep(5000);
+            if(token.IsCancellationRequested)
+                Console.WriteLine("cancellation requested");
+            else
+                Console.WriteLine("no cancellation");
+            
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url, token);
+            this.imageBytes = await response.Content.ReadAsByteArrayAsync(); 
+            
+            if(imageBytes != null)
+            {
+                Console.WriteLine("Downloaded image");
+            }
+            //var result = httpClient.GetByteArrayAsync(url);
+            //this.imageBytes = result.Result;
+
+        }
+
+        public void SaveImage(string filePath)
+        {
+            Console.WriteLine("Saving image....");
+
+            using(Stream fr = new FileStream(filePath, FileMode.Create))
+            {
+                fr.Write(this.imageBytes, 0, this.imageBytes.Length);
+            }
+
+            Console.WriteLine("Successfully saved image.");
+        }
+    }
+}
---
+++ Practice2/ImageDownloader/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Images
+{
+    class Program
+    {
+        private static string imageUrl = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg";
+        private static string directoryPath = @"C:\Users\user\Desktop\Images";
+        private const string DEFAULT_FILE_NAME = "image.jpg";
+        private static string sourceFileName = "imageSource.txt";
+        private static CancellationTokenSource cts;
+        static async Task Main(string[] args)
+        {
+            cts = new CancellationTokenSource();
+            ImageDownloader imageDownloader = new ImageDownloader();
+            Task cancelTask = Task.Run(() => {
+                while (!Console.KeyAvailable)
+                {
+                    if (Console.ReadKey().Key == ConsoleKey.Enter)
+                        cts.Cancel();
+                }
+            });
+            Task downloadTask = imageDownloader.DownloadImageAsync(imageUrl, cts.Token);
+            cts.CancelAfter(5800);
+            try
+            {
+                await downloadTask;
+            }
+            catch (TaskCanceledException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            //await Task.WhenAll(cancelTask, downloadTask);
+
+            //await StartDownload(imageUrl);
+
+            //var imgSource = GetImageURLs(sourceFileName);
+            //try
+            //{
+            //    await StartMultipleDownloads(imgSource);
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine("Something went wrong.");
+            //}
+
+            Console.WriteLine("Completed!");
+           // Console.ReadLine();
+        }
+
+        static IEnumerable<string> GetImageURLs(string fileName)
+        {
+            //string[] urls = File.ReadAllLines(fileName);
+            //return urls;
+
+            List<string> source = new List<string>();
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                while (!sr.EndOfStream)
+                {
+                    source.Add(sr.ReadLine());
+                }
+            }
+            return source;
+        }
+
+        static async Task StartMultipleDownloads(IEnumerable<string> urls)
+        {
+            ImageDownloader imgDownloader = new ImageDownloader();
+
+            List<Task> allTasks = new List<Task>();
+            foreach (string url in urls)
+            {
+                Task task = StartDownload(url);
+                allTasks.Add(task);
+            }
+            Task resultTask = Task.WhenAny(allTasks);
+            try
+            {
+                await resultTask;
+                Console.WriteLine("At least one image is saved");
+            }
+            catch (Exception ex)
+            {
+                foreach (Exception innerEx in resultTask.Exception.InnerExceptions)
+                {
+                    Console.WriteLine(innerEx.Message);
+                }
+            }
+            //try
+            //{
+            //    foreach (var url in urls)
+            //    {
+            //        await StartDownload(url);
+            //    }
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+        }
+
+        static async Task StartDownload(string url)
+        {
+            ImageDownloader imgDownloader = new ImageDownloader();
+            await imgDownloader.DownloadImageAsync(url);
+
+            var di = Directory.CreateDirectory(directoryPath);
+            string imageName = ExtractImageName(url);
+            //string imageName = GenerateImageFileName(url);
+            string filePath = Path.Combine(di.FullName, imageName);
+            imgDownloader.SaveImage(filePath);
+        }
+
+        static string ExtractImageName(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+                return DEFAULT_FILE_NAME;
+
+            int lastIndex = url.LastIndexOf('/');
+            return url.Substring(lastIndex + 1);
+        }
+
+        static string GenerateImageFileName(string url)
+        {
+            var guid = new Guid();
+            string dt = DateTime.Now.ToString("MMddyyyyHHmmssmmmm");
+            return $"{guid}.jpg";
+        }
+    }
+}
