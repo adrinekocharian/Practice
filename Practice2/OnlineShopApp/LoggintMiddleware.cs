4ca9326adb7ccb69a4eef39d0679527eaace5095
adrine <adrinekocharian@gmail.com>
2021-05-09 15:10:49 +0400

    Changed middleware name

--- Practice2/OnlineShopApp/LoggintMiddleware.cs
+++ Practice2/OnlineShopApp/LoggintMiddleware.cs
@@ -8,30 +8,30 @@ using System.Threading.Tasks;
 
 namespace OnlineShopApp
 {
-    public static class CustomMiddlwareExtension
+    public static class LoggintMiddlewareExtension
     {
-        public static void UseCustomMiddleWare(this IApplicationBuilder appBuilder)
+        public static void UseLoggintMiddleware(this IApplicationBuilder appBuilder)
         {
-            appBuilder.UseMiddleware<CustomMIddleware>();
+            appBuilder.UseMiddleware<LoggintMiddleware>();
         }
     }
 
-    public class CustomMIddleware
+    public class LoggintMiddleware
     {
-        public CustomMIddleware(RequestDelegate next, ILogger<CustomMIddleware> logger)
+        public LoggintMiddleware(RequestDelegate next, ILogger<LoggintMiddleware> logger)
         {
-            this.Next = next;
+            this.next = next;
             this.Logger = logger;
         }
-        public RequestDelegate Next { get; set; }
-        public ILogger<CustomMIddleware> Logger { get; set; }
+        private readonly RequestDelegate next;
+        public ILogger<LoggintMiddleware> Logger { get; set; }
         public async Task Invoke(HttpContext httpcontext)
         {
             var header = httpcontext.Request.Headers["Accept-Encoding"];
             //Console.WriteLine(header);
             Logger.LogInformation($"{DateTime.Now} accept-encodeing = {header}");
 
-            await Next.Invoke(httpcontext);
+            await next.Invoke(httpcontext);
         }
     }
 }
